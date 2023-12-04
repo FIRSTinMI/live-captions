da88d2343c00928ed4d7c6ccf43d0797be510d09
Filip Kin <me@filipkin.com>
2023-12-04 00:08:42 -0500

    works

--- public/js/index.js
+++ public/js/index.js
@@ -1,34 +1,106 @@
-
 const lc = document.getElementById('lc');
 const text = document.getElementById('lc-text')
+
+let timeout = 10e3;
+
+function updateConfig() {
+    fetch('/config')
+        .then(res => res.json())
+        .then(json => {
+            // Clear old styles
+            lc.style.removeProperty('bottom');
+            lc.style.removeProperty('top');
+            lc.style.removeProperty('left');
+            lc.style.removeProperty('right');
+            lc.style.removeProperty('transform');
+
+            switch (json.display.position) {
+                case '0':
+                    lc.style.bottom = '0';
+                    break;
+                case '1':
+                    lc.style.top = '0';
+                    break;
+                case '2':
+                    lc.style.bottom = '128px';
+                    break;
+                case '3':
+                    lc.style.top = '128px';
+                    break;
+            }
+            switch (json.display.align) {
+                case 'left':
+                    lc.style.left = '0';
+                    break;
+                case 'right':
+                    lc.style.right = '0';
+                    break;
+                case 'center':
+                    lc.style.left = '50%';
+                    lc.style.transform = 'translateX(-50%)';
+                    break;
+            }
+            text.style.fontSize = json.display.size + 'px';
+            text.style.lineHeight = (parseFloat(json.display.size) + 6) + 'px';
+            lc.style.maxHeight = (json.display.lines * (parseFloat(json.display.size) + 6)) + 'px';
+            timeout = json.display.timeout * 1000;
+        });
+}
 
 // Open connection
 const socket = new WebSocket("ws://localhost:3000/ws/");
 
 // Connection opened
 socket.addEventListener("open", (event) => {
-    socket.send("Connected");
+    console.log('Connected');
+    lc.style.display = 'inline-block';
+    text.innerText = 'Connected';
+    setTimeout(() => {
+        if (!finalMessageOverwritten) {
+            text.innerText = '';
+            lc.style.display = 'none';
+        }
+    }, 5e3);
 });
 
+function capitalize(text) {
+    const arr = text.split(".");
+
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+
+    return arr.join(".");
+}
+
 // Listen for messages
+let finalMessageOverwritten = false;
+let transcript = '';
+let currentTimeout;
 socket.addEventListener("message", (evt) => {
+    let frame = JSON.parse(evt.data);
+    console.log(frame);
+    if (frame.type == 'config') return updateConfig();
+    if (frame.text == '') return;
+
+    finalMessageOverwritten = true;
+    clearTimeout(currentTimeout);
     lc.style.display = 'inline-block';
-    text.innerText = evt.data;
-    setTimeout(() => {
-        text.innerText = '';
-        lc.style.display = 'none';
-    }, 10e3); // TODO: set timeout parameter
+    text.innerText = transcript + capitalize(frame.text.trim());
+    if (frame.isFinal) {
+        finalMessageOverwritten = false;
+        transcript += capitalize(frame.text.trim()) + '. '
+        currentTimeout = setTimeout(() => {
+            text.innerText = '';
+            transcript = '';
+            lc.style.display = 'none';
+        }, timeout);
+    }
 });
 
 const observer = new MutationObserver((mutationList, observer) => {
     lc.scrollTop = lc.scrollHeight;
 });
-observer.observe(text, { attributes: true, childList: true, subtree: true });
+observer.observe(lc, { attributes: true, childList: true, subtree: true });
 
-const searchParams = new URLSearchParams(window.location.href.split('?')[1]);
-if (searchParams.has('position')) {
-    if (searchParams.get('position') == 1) document.getElementById('lc').style.top = '0';
-}
-if (searchParams.has('lines')) {
-    document.getElementById('lc').style.maxHeight = (searchParams.get('lines') * 1.4) + 'em';
-}
+updateConfig();
--- server.js
+++ server.js
@@ -1,42 +1,30 @@
-const PORT = 3000;
-const MODEL = 'large-v3';
-const PROGRAM_FOLDER = './'; //process.env.APPDATA + '\\captions';
-
-const wget = require('wget-improved');
-const cliProgress = require('cli-progress');
-const net = require('net');
-const fs = require('fs');
-
+const recorder = require('node-record-lpcm16');
+const Speech = require('@google-cloud/speech');
 const express = require('express');
 const app = express();
 const expressWs = require('express-ws')(app);
+const { load, save } = require('./util/config');
 
-if (!fs.existsSync(PROGRAM_FOLDER)) {
-    fs.mkdirSync(PROGRAM_FOLDER);
-    console.log('Created ' + PROGRAM_FOLDER);
-}
+const config = load();
 
-// Download model if not exists
-let downloadBar = new cliProgress.SingleBar({ format: '[{bar}] {percentage}% | ETA: {eta}s | {value}/{total} KB', etaBuffer: 10000 }, cliProgress.Presets.shades_classic);
-let size = 0;
-if (!fs.existsSync(`${PROGRAM_FOLDER}/${MODEL}.model`)) {
-    console.log(`Downloading https://huggingface.co/Systran/faster-whisper-${MODEL}/resolve/main/model.bin?download=true`);
-    let download = wget.download(`https://huggingface.co/Systran/faster-whisper-${MODEL}/resolve/main/model.bin?download=true`, `${PROGRAM_FOLDER}/${MODEL}.model`);
-    download.on('error', console.error);
-    download.on('start', (fileSize) => {
-        size = Math.floor(fileSize / 1024 / 1024 * 100) / 100;
-        downloadBar.start(size, 0);
-    });
-    download.on('progress', (progress) => {
-        typeof progress === 'number';
-        downloadBar.update(Math.floor(progress * size * 100) / 100);
-    });
-    download.on('end', () => {
-        downloadBar.stop();
-    });
-}
+const speech = new Speech.SpeechClient(config.google);
 
 app.use(express.static('public'));
+
+app.get('/config', (req, res) => {
+    res.send({ display: config.display, server: config.server });
+});
+
+app.post('/config/:setting', (req, res) => {
+    console.log(req.params.setting + ': ' + req.query.value);
+    let setting = req.params.setting.split('.');
+    config[setting[0]][setting[1]] = req.query.value;
+    save(config);
+    for (let ws of clients) {
+        ws.send(JSON.stringify({ type: 'config' }));
+    }
+    res.send();
+});
 
 let clients = [];
 
@@ -45,25 +33,56 @@ app.ws('/ws/', (ws, req) => {
 
     ws.on('message', (msg) => {
         console.log(msg);
-        ws.send(msg);
     });
 
     console.log('new connection to websocket');
 });
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
+app.listen(config.server.port, () => {
+    console.log(`Open captions http://127.0.0.1:${config.server.port}\nOpen settings http://127.0.0.1:${config.server.port}/settings.html`);
 });
 
-// TCP server for communicating with whisper process
-net.createServer((socket) => {
-    console.log('new connection to tcp socket');
+const request = {
+    config: {
+        encoding: 'LINEAR16',
+        sampleRateHertz: config.server.sampleRate,
+        languageCode: 'en-US',
+        model: 'latest_long'
+    },
+    interimResults: true,
+};
 
-    socket.on('data', function (data) {
-        let msg = data.toString();
-        console.log(msg);
-        for (let client of clients) {
-            client.send(msg);
-        }
-    });
-}).listen(65432);
+function startStreaming() {
+    const recognizeStream = speech
+        .streamingRecognize(request)
+        .on('error', (err) => {
+            console.error(err);
+            if (err.code == 11) return startStreaming();
+        })
+        .on('data', data => {
+            let frame = {
+                type: 'words',
+                isFinal: data.results[0].isFinal,
+                text: data.results[0].alternatives[0].transcript,
+                confidence: data.results[0].alternatives[0].confidence
+            }
+            let msg = JSON.stringify(frame);
+            for (let ws of clients) {
+                ws.send(msg);
+            }
+        });
+
+    recorder
+        .record({
+            sampleRateHertz: config.sampleRate,
+            threshold: 0,
+            verbose: false,
+            recordProgram: 'rec',
+            silence: '10.0',
+        })
+        .stream()
+        .on('error', console.error)
+        .pipe(recognizeStream);
+}
+
+startStreaming();
